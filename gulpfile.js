bdd556291d66e541ba824d74514de8b4c783eecc
Valentin Paul <valentin.paul@etu.utc.fr>
2016-08-15 12:14:55 +0200

    implementation of new design for the website

--- gulpfile.js
+++ gulpfile.js
@@ -22,25 +22,26 @@ gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
 
 // Less task to compile the less files and add the banner
 gulp.task('less', function() {
-    return gulp.src('less/creative.less')
+    return gulp.src('less/*.less')
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
 // Minify CSS
 gulp.task('minify-css', function() {
-    return gulp.src('css/creative.css')
+    return gulp.src('css/*.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
+
 
 // Minify JS
 gulp.task('minify-js', function() {
@@ -51,32 +52,27 @@ gulp.task('minify-js', function() {
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
 // Copy Bootstrap core files from node_modules to vendor directory
 gulp.task('bootstrap', function() {
     return gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor/bootstrap'))
-})
+        .pipe(gulp.dest('vendor/bootstrap'));
+});
 
 // Copy jQuery core files from node_modules to vendor directory
 gulp.task('jquery', function() {
     return gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor/jquery'))
-})
+        .pipe(gulp.dest('vendor/jquery'));
+});
 
-// Copy Magnific Popup core files from node_modules to vendor directory
-gulp.task('magnific-popup', function() {
-    return gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('vendor/magnific-popup'))
-})
 
 // Copy ScrollReveal JS core JavaScript files from node_modules
 gulp.task('scrollreveal', function() {
     return gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('vendor/scrollreveal'))
-})
+        .pipe(gulp.dest('vendor/scrollreveal'));
+});
 
 // Copy Font Awesome core files from node_modules to vendor directory
 gulp.task('fontawesome', function() {
@@ -88,11 +84,11 @@ gulp.task('fontawesome', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
-})
+        .pipe(gulp.dest('vendor/font-awesome'));
+});
 
 // Copy all dependencies from node_modules
-gulp.task('copy', ['bootstrap', 'jquery', 'fontawesome', 'magnific-popup', 'scrollreveal']);
+gulp.task('copy', ['bootstrap', 'jquery', 'fontawesome', 'scrollreveal']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -100,8 +96,8 @@ gulp.task('browserSync', function() {
         server: {
             baseDir: ''
         },
-    })
-})
+    });
+});
 
 // Watch Task that compiles LESS and watches for HTML or JS changes and reloads with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
